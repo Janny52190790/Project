fd66e0c3ca92e7a6822350ddf2c857806f580f6a
Janny52190790 <106797053+Janny52190790@users.noreply.github.com>
2022-11-21 21:35:45 -0800

    Delete asgn7 directory

--- asgn7/metric.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef enum { EUCLIDEAN, MANHATTAN, COSINE } Metric;
-
-static const char *metric_names[] = { [EUCLIDEAN] = "Euclidean distance",
-    [MANHATTAN] = "Manhattan distance",
-    [COSINE] = "Cosine distance" };
